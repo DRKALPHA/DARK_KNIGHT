234b29217dbcf8fd40019d30800e5fb0756705b4
DRK DEVELOPER <146550095+DRKALPHA@users.noreply.github.com>
2024-11-17 20:28:05 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,20 +13,16 @@ try{
 
 let des = `👋 𝙷𝚎𝚕𝚕𝚘 ${pushname} 𝙸'𝚖 𝚊𝚕𝚒𝚟𝚎 𝚗𝚘𝚠
 
-*Im NETHU-MD Whatsapp Bot Create By Nethu Max 🍂✨*
+*Im DARK KNIGHT Whatsapp Bot Create By Kavindu Suranga 🍂✨*
 
 | *Version*: 1.0.0
 | *Memory*: 38.09MB/7930MB
 | *Runtime*: 1 minute,25 seconds
-| *Owner*: Nethmika
+| *Developer*: 𝙺𝙰𝚅𝙸𝙽𝙳𝚄 𝚂𝚄𝚁𝙰𝙽𝙶𝙰
 
-I am nethu md whatsapp bot. How can I help you.
-To get the menu, type as menu. If you need to know something about the bot,
-type as owner and direct the question to me. Good day.
 
-*ꜱʟ°᭄🇱🇰™️𝙳𝙰𝚁𝙺 𝙲𝚈𝙱𝙴𝚁*
 
- ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴜ ᴍᴀx`
+ ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙺𝙰𝚅𝙸𝙽𝙳𝚄 𝚂𝚄𝚁𝙰𝙽𝙶𝙰`
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
 }catch(e){
 console.log(e)
