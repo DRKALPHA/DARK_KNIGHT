cf47c1e49c525447633871f70f462015de238e53
DRK DEVELOPER <146550095+DRKALPHA@users.noreply.github.com>
2024-11-17 19:50:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IvcgxZAa#C7wAUz6FPdqhk5bCUZkDYm8ZfNw6EuLcjkDnpk3UWe4",
+SESSION_ID: process.env.SESSION_ID || "bJ0ylAyC#-Mp6NcIajCPFtLL5QmXYAlwCH2IMGGa2e7l1rsbFIm0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/7f0d7a04a30a602307e3d.jpg",
-SUDO_NB: process.env.SUDO_NB || "94715922193",
+SUDO_NB: process.env.SUDO_NB || "94713824099",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: "true"
