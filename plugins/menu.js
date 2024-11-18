c315e0864e970da4184eda335fc8152d19d7cc6b
DRK DEVELOPER <146550095+DRKALPHA@users.noreply.github.com>
2024-11-18 16:59:47 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -14,10 +14,10 @@ try{
 let dec = `
 
 ╭─────────────━┈⊷
-│👾 ʙᴏᴛ ɴᴀᴍᴇ: ɴᴇᴛʜᴜ ᴍᴅ
-│👨‍💻 ᴏᴡɴᴇʀ : ɴᴇᴛʜᴍɪᴋᴀ ᴋᴀᴜꜱʜᴀʟʏᴀ    
-│👤 ɴᴜᴍʙᴇʀ: 94704227534
-│💻 HOSTER: ɴᴇᴛʜᴜ ᴍᴀx
+│👾 ʙᴏᴛ ɴᴀᴍᴇ: ᴅᴀʀᴋ ᴋɴɪɢʜᴛ
+│👨‍💻 ᴏᴡɴᴇʀ : ${pushname}
+│👤 ɴᴜᴍʙᴇʀ: 94713824099
+│💻 HOSTER: ᴋᴀᴠɪɴᴅᴜ ꜱᴜʀᴀɴɢᴀ
 │💫 ᴘʀᴇғɪx: [Multi-Prefix]
 ╰─────────────━┈⊷ 
 ╭━❮ 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙴𝚁 ❯━╮
@@ -55,12 +55,12 @@ let dec = `
 ┃✰ .𝙰𝚕𝚒𝚟𝚎
 ┃✰ .𝙾𝚠𝚗𝚎𝚛
 ┃✰ .𝙼𝚎𝚗𝚞
-|✰ .𝚁𝚎𝚙𝚘
+┃✰ .𝚁𝚎𝚙𝚘
 ╰━━━━━━━━━━━━━━━⪼
 
- ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴜ ᴍᴀx
+ ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅʀᴋ ᴅᴇᴠᴇʟᴏᴘᴇʀ
 `
-await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/7f0d7a04a30a602307e3d.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://www.imgtr.net/cT2itnlgWawGfXA`},caption:dec},{quoted:mek});
 
 }catch(e){
 console.log(e)
