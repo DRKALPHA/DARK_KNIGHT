93fafed0fbf86574168f49e9a540f3653eaf60bf
DRK DEVELOPER <146550095+DRKALPHA@users.noreply.github.com>
2024-11-19 06:48:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `DARK-KNIGHT connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/6d986fa37cb22f8e6717a.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://www.imgtr.net/ib/EA8pVTuImefPsch_1731927827.jpg` }, caption: up })
 
 }
 })
