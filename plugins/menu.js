c754b9f53cfb9ce6fb56528dab2f4ab2fe227452
DRK DEVELOPER <146550095+DRKALPHA@users.noreply.github.com>
2024-11-19 22:17:57 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -22,8 +22,7 @@ let dec = `
 |⏳ ᴜᴘ ᴛɪᴍᴇ : ${runtime(process.uptime())}    
 |📟 ʀᴀᴍ ᴜꜱᴀɢᴇ : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 |⚙️ ʜɪꜱᴛ ɴᴀᴍᴇ : ${os.hostname()}
-│💻 ʜᴏꜱᴛᴇʀ : ᴋᴀᴠɪɴᴅᴜ ꜱᴜʀᴀɴɢᴀ
-│💫 ᴘʀᴇꜰɪx : ` . `
+│💻 ʜᴏꜱᴛᴇʀ : ᴋᴀᴠɪɴᴅᴜ ꜱᴜʀᴀɴɢᴀ`
 ╰─────────────━┈⊷ 
 ╭━❮ 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙴𝚁 ❯━╮
 ┃✰ .𝚂𝚝𝚒𝚌𝚔𝚎𝚛
